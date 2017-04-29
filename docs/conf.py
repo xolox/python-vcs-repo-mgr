496a2a437dc2a55851eddacb03690d775481c5dc
Peter Odding <peter@peterodding.com>
2017-04-29 02:10:47 +0200

    Change Sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ intersphinx_mapping = {
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'nature'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'vcsrepomgrdoc'
