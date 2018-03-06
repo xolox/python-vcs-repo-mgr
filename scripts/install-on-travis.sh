3687c028c6f286803cac013def62db66c4fc25a2
Peter Odding <peter@peterodding.com>
2018-03-06 18:43:35 +0100

    Run the test suite on Mac OS X*
    While I never specifically intended for vcs-repo-mgr to be used on Apple
    systems, a colleague of mine has been trying to do exactly this and has
    run into a number of issues that are probably caused by platform
    incompatibilities in vcs-repo-mgr and/or its dependencies.
    
    I'm not sure whether it's even feasible to get the vcs-repo-mgr test
    suite running on Mac OS X on Travis CI so I'm considering just diving in
    and fixing vcs-repo-mgr and/or test suite incompatibilities as I go...

--- scripts/install-on-travis.sh
+++ scripts/install-on-travis.sh
@@ -1,16 +1,28 @@
 #!/bin/bash -e
 
-# Make sure apt-get's package lists are up-to-date.
-sudo apt-get update -qq
+# On Linux workers we need to install some requirements.
+if [ "$TRAVIS_OS_NAME" = linux ]; then
+  # Make sure apt-get's package lists are up-to-date.
+  sudo apt-get update -qq
+  # Make sure git is installed.
+  sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes git
+  # Install up-to-date versions of Bazaar and Mercurial
+  # (using Python packaging instead of Debian packaging).
+  if python -c 'import sys; sys.exit(1 if sys.version_info[0] == 2 else 0)'; then
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes python-pip
+    sudo pip install --upgrade bzr mercurial
+  fi
+fi
 
-# Make sure git is installed.
-sudo apt-get install --yes git
-
-# Install up-to-date versions of Bazaar and Mercurial
-# (using Python packaging instead of Debian packaging).
-if python -c 'import sys; sys.exit(1 if sys.version_info[0] == 2 else 0)'; then
-  sudo apt-get install --yes python-pip
-  sudo pip install --upgrade bzr mercurial
+# On Mac OS X workers we are responsible for creating the Python virtual
+# environment, because we set `language: generic' in the Travis CI build
+# configuration file (to bypass the lack of Python runtime support).
+if [ "$TRAVIS_OS_NAME" = osx ]; then
+  VIRTUAL_ENV="$HOME/virtualenv/python2.7"
+  if [ ! -x "$VIRTUAL_ENV/bin/python" ]; then
+    virtualenv "$VIRTUAL_ENV"
+  fi
+  source "$VIRTUAL_ENV/bin/activate"
 fi
 
 # Install the required Python packages.
