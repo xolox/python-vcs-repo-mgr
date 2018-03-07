b1a11b51346739e13e754b1dc8fb9bb7e33cd99a
Peter Odding <peter@peterodding.com>
2018-03-08 00:16:56 +0100

    Release 4.1: Fix for issue #4, document MacOS compatibility

--- vcs_repo_mgr/__init__.py
+++ vcs_repo_mgr/__init__.py
@@ -1,7 +1,7 @@
 # Version control system repository manager.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: March 5, 2018
+# Last Change: March 8, 2018
 # URL: https://github.com/xolox/python-vcs-repo-mgr
 
 """
@@ -111,7 +111,7 @@ from vcs_repo_mgr.exceptions import (
 )
 
 # Semi-standard module versioning.
-__version__ = '4.0'
+__version__ = '4.1'
 
 USER_CONFIG_FILE = '~/.vcs-repo-mgr.ini'
 """The location of the user-specific configuration file (a string, parsed using :func:`.parse_path()`)."""
